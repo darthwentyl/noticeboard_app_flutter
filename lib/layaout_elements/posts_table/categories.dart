7f9ba1131138c33601bf7d473893eff86a9def00
Michal Wendland <wendland.michal@gmail.com>
2022-11-30 14:55:08 +0100

    - refactor categories widget

--- lib/layaout_elements/posts_table/categories.dart
+++ lib/layaout_elements/posts_table/categories.dart
@@ -2,33 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:noticeboard/const/app_colors.dart';
 import 'package:noticeboard/const/app_strings.dart';
 import 'package:noticeboard/const/app_theme.dart';
+import 'package:noticeboard/utils/size_getter.dart';
 
 class Categories extends StatelessWidget {
   const Categories({Key? key}) : super(key: key);
 
-  static const spacing = 8.0;
+  static const spacing = 6.0;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
+    return Center(
       child: Container(
+        width: SizeGetter.width(context, 98.0),
         decoration: const BoxDecoration(
           color: AppColors.boxBackground,
           borderRadius: BorderRadius.all(Radius.circular(BoxTheme.radius)),
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: spacing, right: spacing),
-          child: Wrap(
-            spacing: spacing,
-            children: [
-              _button(ButtonStrings.categoriesPost),
-              _button(ButtonStrings.categoriesBuySell),
-              _button(ButtonStrings.categoriesAdverisment),
-              _button(ButtonStrings.categoriesContest),
-              _button(ButtonStrings.categoriesVip),
-              _button(ButtonStrings.categoriesInfo),
-            ],
+        child: Center(
+          child: Container(
+            width: SizeGetter.width(context, 95.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Wrap(
+                spacing: spacing,
+                children: [
+                  _button(ButtonStrings.categoriesPost),
+                  _button(ButtonStrings.categoriesBuySell),
+                  _button(ButtonStrings.categoriesAdverisment),
+                  _button(ButtonStrings.categoriesContest),
+                  _button(ButtonStrings.categoriesVip),
+                  _button(ButtonStrings.categoriesInfo),
+                ],
+              ),
+            ),
           ),
         ),
       ),
